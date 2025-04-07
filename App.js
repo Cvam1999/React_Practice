552d1b0bfad4d222d29a39bb184bc56b9d0bb656
Cvam1999 <shivamgupta019902@gmail.com>
2025-04-07 01:08:38 -0700

    dependencies

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-const heading = React.createElement('h1',{ id :"heading"},"hello world shivam");
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const heading = React.createElement('h1',{ id :"heading"},"hello world shivam Gupta👌✌");
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
 
