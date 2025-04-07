71c25ef4cfdab7b627dd7eb7c3d4048614c5607a
Cvam1999 <shivamgupta019902@gmail.com>
2025-04-06 23:19:29 -0700

    changes

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
 const heading = React.createElement('h1',{ id :"heading"},"hello world shivam");
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
+
+// shivam
